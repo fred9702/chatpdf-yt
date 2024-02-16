d9b7bbcfc03c0d4b02ec44e194c98a02a2182419
Fred Lawrence <fred9702@gmail.com>
2024-02-16 15:59:56 +0100

    Home Page styling: <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"> ---- This centres everything
    Added "/" as a public route in middleware.ts
    
    Created sign-up & sign-in pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,42 @@
 import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
+import { LogInIcon } from "lucide-react";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const {userId} = await auth();
+  const isAuth = !!userId;
+
   return (
-    <Button>Click Me</Button>);
+    <div className="w-screen min-h-screen bg-gradient-to-r bg-gradient-to-l from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+          
+          <p className="max-w-xl mt-2 text-lg text-slate-600">
+            Join millions of students, researchers and professionals to instantly answer questions and understand research with AI
+          </p>
+          
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>File Upload</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>Login to get started
+                  <LogInIcon />
+                </Button>
+              </Link>
+            )
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
